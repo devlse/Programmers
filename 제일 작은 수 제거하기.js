5fa9d56e5d4d7d72a90295fe0895a269ea31cbfb
devlse <59230@naver.com>
2022-01-18 23:49:57 +0900

    제일 작은 수 제거하기

--- 제일 작은 수 제거하기.js
+++ 제일 작은 수 제거하기.js
@@ -1,5 +1,4 @@
 function solution(arr) {
-    arr.splice(arr.indexOf(Math.min(...arr)),1);
-    if(arr.length<1)return[-1];
+    arr.length <= 1 ? arr = [-1] : arr.splice(arr.indexOf(Math.min(...arr)), 1);
     return arr;
 }
